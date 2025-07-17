548fcbcafe4cc7c8aae40cf2810a60c0efd4b68b
LexaBoxan <80101881+LexaBoxan@users.noreply.github.com>
2025-07-17 15:08:01 +0700

    Restructure package and add gitignore

--- seeding/models/data_models.py
+++ seeding/models/data_models.py
@@ -1,9 +1,13 @@
 # models/dataclasses_me.py
+"""
+Dataclasses for хранение изображений и результатов детекции.
+"""
 
 from dataclasses import dataclass, field
 from typing import List, Optional, Union
 import numpy as np
 from PIL import Image
+
 
 @dataclass
 class AllClassImage:
@@ -15,9 +19,11 @@ class AllClassImage:
         confidence (float): Уверенность модели в принадлежности к классу (0.0 - 1.0).
         image (Union[np.ndarray, Image.Image]): Картинка участка, соответствующего данному классу.
     """
+
     class_name: str
     confidence: float
     image: Union[np.ndarray, Image.Image]
+
 
 @dataclass
 class ObjectImage:
@@ -30,6 +36,7 @@ class ObjectImage:
         image (List[Union[np.ndarray, Image.Image]]): Список изображений, соответствующих объекту.
         image_all_class (Optional[List[AllClassImage]]): Подклассы, выделенные внутри объекта.
     """
+
     class_name: str
     confidence: float
     image: List[Union[np.ndarray, Image.Image]] = field(default_factory=list)
@@ -49,9 +56,9 @@ class OriginalImage:
         final_images (List[Union[np.ndarray, Image.Image]]): Список итоговых изображений после обработки.
         class_object_image (Optional[List[ObjectImage]]): Список объектов, выделенных на изображениях.
     """
-    file_path: str = ''
+
+    file_path: str = ""
     images: List[Union[np.ndarray, Image.Image]] = field(default_factory=list)
     masks: List[Union[np.ndarray, Image.Image]] = field(default_factory=list)
     final_images: List[Union[np.ndarray, Image.Image]] = field(default_factory=list)
     class_object_image: Optional[List[ObjectImage]] = None
-
